77cbb25ddae13010a4890f95f70db340489eb71c
Catalina <caliste@fen.uchile.cl>
2021-06-23 00:22:26 -0400

    Fix db - no delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_215742) do
+ActiveRecord::Schema.define(version: 2021_06_22_215303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,5 +80,6 @@ ActiveRecord::Schema.define(version: 2021_06_22_215742) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "quotations", "clients"
   add_foreign_key "quotations", "users"
 end
