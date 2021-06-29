76fffcccd7d2f8e7d695caafb08aa6fa36652c6b
Catalina <caliste@fen.uchile.cl>
2021-06-29 18:35:11 -0400

    Fix: heroku assets

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,9 @@ module ClaudioGajardo
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
+     config.assets.initialize_on_precompile = false
      config.time_zone = "Santiago"
+     config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
     
   end
